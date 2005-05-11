ca38c11dff84e797ca15a3b058915f0c449240b2
Gregory Brown <gregory.t.brown@gmail.com>
2005-05-11 20:46:54 +0000

    Cut out a test that wasn't doing anything

--- highline/lib/highline/menu.rb
+++ highline/lib/highline/menu.rb
@@ -4,20 +4,6 @@
 #  Copyright 2005 smtose.org. All rights reserved.
 class HighLine
 	class Menu
-		class Choice
-	
-			def initialize(name, action)
-				@name = name
-				@action = action
-			end
-
-			attr_reader :name
-	
-			def act()
-				@action.nil? ? self.name : @action.call
-			end
-		end
-
 		def initialize()
 			@items = []
 			@index = :number
@@ -26,11 +12,11 @@ class HighLine
 		end
 	
 		def choice(name,&action)
-			@items << Choice.new(name,action)
+			@items << [name,action]
 		end
 
 		def find(name)
-			@items.find { |c| c.name == name }
+			@items.find { |c| c.first == name }
 		end
 	
 		def remove(name)
@@ -41,20 +27,20 @@ class HighLine
 		def display
 			menu_string = ""
 			if self.index.eql?(:number)
-				menu_string << (self.items.map { |c| "#{self.items.index(c)+1}. #{c.name}\n" }).to_s
+				menu_string << (@items.map { |c| "#{self.items.index(c)+1}. #{c.first}\n" }).to_s
 			elsif self.index.eql?(:letter)
 				l_index = "`"
-				menu_string << (self.items.map { |c| "#{l_index.succ!}. #{c.name}\n" }).to_s
+				menu_string << (@items.map { |c| "#{l_index.succ!}. #{c.first}\n" }).to_s
 			elsif self.index.eql?(:none)
-				menu_string << (self.items.map { |c| "- #{c.name}\n" }).to_s
+				menu_string << (@items.map { |c| "- #{c.first}\n" }).to_s
 			end
 			return menu_string
 		end
 
    		def options()
     	 		options = []
-    	 		by_index = lambda { (1 .. self.items.size).collect { |s| String(s) } }
-     			by_name  = lambda { self.items.collect { |c| c.name } }
+    	 		by_index = lambda { (1 .. @items.size).collect { |s| String(s) } }
+     			by_name  = lambda { self.items.collect { |c| c.first } }
     	 		options = case self.select_by
         	        	when :index then
         	           	  by_index.call
@@ -65,6 +51,19 @@ class HighLine
         	        end
      			return options
    		end
+
+		def select(user_input)
+			
+			if user_input =~ /^\d+$/ 
+				tuple = @items[user_input.to_i-1]
+				tuple.last.nil? ? tuple.first : tuple.last.call
+			
+			else
+      				tuple = find(user_input)
+				tuple.last.nil? ? tuple.first : tuple.last.call
+    			end
+		end
+			
 
 		def choices( *names, &action )
     			names.each { |n| choice(n, &action) }
