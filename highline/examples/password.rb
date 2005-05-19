ffcbcff392547693eeffcc5fc3da132478a86308
James Gray <james@grayproductions.net>
2005-05-19 17:18:35 +0000

    Hacking in termios for testing...

--- highline/examples/password.rb
+++ highline/examples/password.rb
@@ -1,6 +1,6 @@
 #!/usr/local/bin/ruby -w
 
-require "rubygems"
+$:.unshift(File.join(File.dirname(__FILE__), "..", "lib"))
 require "highline/import"
 
 pass = ask("Enter your password:  ") { |q| q.echo = false }
