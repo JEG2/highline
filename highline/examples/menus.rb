a46320bdf8e697461a8e335f7824ecf34b9ad3a0
Gregory Brown <gregory.t.brown@gmail.com>
2005-05-20 17:19:56 +0000

    some menu examples

--- highline/examples/menus.rb
+++ highline/examples/menus.rb
@@ -5,7 +5,7 @@ require "highline/import"
 
 # The old way, using ask() and say()...
 choices = %w{ruby python perl}
-
+say("This is the old way using ask() and say():")
 say("Please choose your favorite programming language:")
 say(choices.map { |c| "  #{c}\n" }.join)
 
@@ -17,10 +17,28 @@ else
 end
 
 # The new and improved choose()...
+say("This is the new mode (default)")
 choose do |menu|
-	menu.prompt = "Please choose your favorite programming language?  "
+	menu.header = "Please choose your favorite programming language"
 	menu.choice :ruby do say("Good choice!") end
 	menu.choices(:python, :perl) do say("Not from around here, are you?") end
 end
 
+say("This is letter indexing")
+choose do |menu|
+	menu.header = "Please choose your favorite programming language"
+	menu.index = :letter
+	menu.choice :ruby do say("Good choice!") end
+	menu.choices(:python, :perl) do say("Not from around here, are you?") end
+end
+
+say("This is without indexing")
+choose do |menu|
+	menu.header = "Please choose your favorite programming language"
+	menu.index = "*"
+	menu.choice :ruby do say("Good choice!") end
+	menu.choices(:python, :perl) do say("Not from around here, are you?") end
+end
+
+	
 
