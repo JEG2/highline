482702ebbf6a1dafe7416e050b8961aba14aa8cd
James Gray <james@grayproductions.net>
2005-05-20 19:40:59 +0000

    Adding Rdoc for menu.rb.

--- highline/lib/highline/import.rb
+++ highline/lib/highline/import.rb
@@ -12,12 +12,12 @@ $terminal = HighLine.new
 
 #
 # <tt>require "highline/import"</tt> adds shorcut methods to Kernel, making
-# agree(), ask(), and say() globally available.  This is handy for quick and
-# dirty input and output.  These methods use the HighLine object in the global
-# variable <tt>$terminal</tt>, which is initialized to used <tt>$stdin</tt> and
-# <tt>$stdout</tt> (you are free to change this).  Otherwise, these methods are
-# identical to their HighLine counterparts, see that class for detailed
-# explinations.
+# agree(), ask(), choose() and say() globally available.  This is handy for
+# quick and dirty input and output.  These methods use the HighLine object in
+# the global variable <tt>$terminal</tt>, which is initialized to used
+# <tt>$stdin</tt> and <tt>$stdout</tt> (you are free to change this).
+# Otherwise, these methods areidentical to their HighLine counterparts, see that
+# class for detailed explinations.
 #
 module Kernel
 	extend Forwardable
