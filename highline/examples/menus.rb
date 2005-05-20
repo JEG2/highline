15dae0a89f3f30920c7a974d6f5d086a6c9a6b4b
Gregory Brown <gregory.t.brown@gmail.com>
2005-05-20 15:56:27 +0000

    menu example

--- highline/examples/menus.rb
+++ highline/examples/menus.rb
@@ -3,14 +3,22 @@
 require "rubygems"
 require "highline/import"
 
-choices = %w{ruby python perl}
+#choices = %w{ruby python perl}
 
-say("Please choose your favorite programming language:")
-say(choices.map { |c| "  #{c}\n" }.join)
+#say("Please choose your favorite programming language:")
+#say(choices.map { |c| "  #{c}\n" }.join)
 
-case ask("?  ", choices)
-when "ruby"
-	say("Good choice!")
-else
-	say("Not from around here, are you?")
+#case ask("?  ", choices)
+#when "ruby"
+#	say("Good choice!")
+#else
+#	say("Not from around here, are you?")
+#end
+
+choose do |menu|
+	menu.header = "Please choose your favorite programming language"
+	menu.choice :ruby do say("Good choice!") end
+	menu.choices(:python, :perl) do say("Not from around here, are you?") end
 end
+
+
