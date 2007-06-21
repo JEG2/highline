767089550cf526e66ff7de4338c8aac8c618b541
James Gray <james@grayproductions.net>
2007-06-21 20:03:33 +0000

    Bumping version and logging changes.

--- lib/highline.rb
+++ lib/highline.rb
@@ -29,7 +29,7 @@ require "abbrev"
 #
 class HighLine
   # The version of the installed library.
-  VERSION = "1.2.8".freeze
+  VERSION = "1.2.9".freeze
   
   # An internal HighLine error.  User code does not need to trap this.
   class QuestionError < StandardError
