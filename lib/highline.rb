9544bec4edf995d371d9fdf7f38814f7a4cfcde9
Stephen Bannasch <stephen.bannasch@gmail.com>
2009-10-06 02:30:17 -0400

    bump version to 1.5.2, includes JRuby support
    need to manually install the ffi-ncurses gem in JRuby
    
      jruby -S gem install ffi-ncurses

--- lib/highline.rb
+++ lib/highline.rb
@@ -31,7 +31,7 @@ require "highline/color_scheme"
 #
 class HighLine
   # The version of the installed library.
-  VERSION = "1.5.1".freeze
+  VERSION = "1.5.2".freeze
   
   # An internal HighLine error.  User code does not need to trap this.
   class QuestionError < StandardError
