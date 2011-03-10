63bc54d9c96aedb9051380963115235430f91763
James Edward Gray II <james@graysoftinc.com>
2011-03-10 10:14:26 -0600

    Trying a fix for the Window loading error.

--- lib/highline/system_extensions.rb
+++ lib/highline/system_extensions.rb
@@ -36,6 +36,8 @@ class HighLine
       # 
       def get_character( input = STDIN )
         Win32API.new("crtdll", "_getch", [ ], "L").Call
+      rescue Exception
+        Win32API.new("msvcrt", "_getch", [ ], "L").Call
       end
 
       # A Windows savvy method to fetch the console columns, and rows.
