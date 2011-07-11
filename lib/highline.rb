07413c7477525f5edaeb4a065b95964cd76f9da4
James Edward Gray II <james@graysoftinc.com>
2011-07-11 09:02:26 -0500

    Updating Changelog.

--- lib/highline.rb
+++ lib/highline.rb
@@ -30,7 +30,7 @@ require "highline/style"
 #
 class HighLine
   # The version of the installed library.
-  VERSION = "1.6.4".freeze
+  VERSION = "1.6.5".freeze
   
   # An internal HighLine error.  User code does not need to trap this.
   class QuestionError < StandardError
