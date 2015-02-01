0eaf9e8c417783995bb43318741a45888ed26591
Abinoam Praxedes Marques Jr <abinoam@gmail.com>
2015-02-01 05:32:44 -0300

    Simplify Style#create_bright_variant

--- lib/highline/style.rb
+++ lib/highline/style.rb
@@ -180,13 +180,9 @@ class HighLine
     def create_bright_variant(variant_name)
       raise "Cannot create a #{name} variant of a style list (#{inspect})" if @list
       new_name = ("#{variant_name}_"+@name.to_s).to_sym
+      new_rgb = @rgb == [0,0,0] ? [128, 128, 128] : @rgb.map {|color|  color==0 ? 0 : [color+128,255].min }
 
-      if style = find_style(new_name)
-        style
-      else
-        new_rgb = @rgb == [0,0,0] ? [128, 128, 128] : @rgb.map {|color|  color==0 ? 0 : [color+128,255].min }
-        variant(new_name, :increment=>60, :rgb=>new_rgb)
-      end
+      find_style(new_name) or variant(new_name, :increment=>60, :rgb=>new_rgb)
     end
 
     def find_style(name)
