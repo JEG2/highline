0d374b767fff71e1520df71bd8678857084e23d3
Abinoam Praxedes Marques Jr <abinoam@gmail.com>
2015-04-27 00:25:44 -0300

    Indent only

--- lib/highline/terminal/unix_stty.rb
+++ lib/highline/terminal/unix_stty.rb
@@ -1,40 +1,40 @@
 class HighLine
   class Terminal::UnixStty < Terminal
 
-      # A Unix savvy method using stty to fetch the console columns, and rows.
-      # ... stty does not work in JRuby
-      def terminal_size
-        begin
-          require "io/console"
-          winsize = IO.console.winsize.reverse rescue nil
-          return winsize if winsize
-        rescue LoadError
-        end
-
-        if /solaris/ =~ RUBY_PLATFORM and
-          `stty` =~ /\brows = (\d+).*\bcolumns = (\d+)/
-          [$2, $1].map { |c| x.to_i }
-        elsif `stty size` =~ /^(\d+)\s(\d+)$/
-          [$2.to_i, $1.to_i]
-        else
-          [ 80, 24 ]
-        end
+    # A Unix savvy method using stty to fetch the console columns, and rows.
+    # ... stty does not work in JRuby
+    def terminal_size
+      begin
+        require "io/console"
+        winsize = IO.console.winsize.reverse rescue nil
+        return winsize if winsize
+      rescue LoadError
       end
 
-                                # *WARNING*:  This requires the external "stty" program!
-      CHARACTER_MODE = "stty"   # For Debugging purposes only.
-
-      def raw_no_echo_mode
-        @state = `stty -g`
-        system "stty raw -echo -icanon isig"
+      if /solaris/ =~ RUBY_PLATFORM and
+        `stty` =~ /\brows = (\d+).*\bcolumns = (\d+)/
+        [$2, $1].map { |c| x.to_i }
+      elsif `stty size` =~ /^(\d+)\s(\d+)$/
+        [$2.to_i, $1.to_i]
+      else
+        [ 80, 24 ]
       end
+    end
 
-      def restore_mode
-        system "stty #{@state}"
-      end
+                              # *WARNING*:  This requires the external "stty" program!
+    CHARACTER_MODE = "stty"   # For Debugging purposes only.
 
-      def get_character( input = STDIN )
-        input.getbyte
-      end
+    def raw_no_echo_mode
+      @state = `stty -g`
+      system "stty raw -echo -icanon isig"
+    end
+
+    def restore_mode
+      system "stty #{@state}"
+    end
+
+    def get_character( input = STDIN )
+      input.getbyte
+    end
   end
 end
