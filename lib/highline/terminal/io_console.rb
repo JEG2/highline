6e1878e1f1ea29149fe454c23786c6947f43ee98
Abinoam Praxedes Marques Jr <abinoam@gmail.com>
2015-07-17 17:47:51 -0300

    Make Terminal::IOConsole work

--- lib/highline/terminal/io_console.rb
+++ lib/highline/terminal/io_console.rb
@@ -3,46 +3,26 @@
 class HighLine
   class Terminal
     class IOConsole < Terminal
-
-      # A Unix savvy method using stty to fetch the console columns, and rows.
-      # ... stty does not work in JRuby
       def terminal_size
-        begin
-          require "io/console"
-          winsize = IO.console.winsize.reverse rescue nil
-          return winsize if winsize
-        rescue LoadError
-        end
-
-        if /solaris/ =~ RUBY_PLATFORM and
-          `stty` =~ /\brows = (\d+).*\bcolumns = (\d+)/
-          [$2, $1].map { |c| x.to_i }
-        elsif `stty size` =~ /^(\d+)\s(\d+)$/
-          [$2.to_i, $1.to_i]
-        else
-          [ 80, 24 ]
-        end
+        IO.console.winsize.reverse
       end
 
-                                # *WARNING*:  This requires the external "stty" program!
-      CHARACTER_MODE = "unix_stty"   # For Debugging purposes only.
+      CHARACTER_MODE = "io_console"   # For Debugging purposes only.
 
       def raw_no_echo_mode
-        @state = `stty -g`
-        system "stty raw -echo -icanon isig"
+        STDIN.echo = false
       end
 
       def restore_mode
-        system "stty #{@state}"
-        print "\r"
+        STDIN.echo = true
       end
 
-      def get_character( input = STDIN )
-        input.getc
+      def get_character(input = STDIN)
+        input.getch # from ruby io/console
       end
 
       def character_mode
-        "unix_stty"
+        "io_console"
       end
 
       def get_line(question, highline, options={})
