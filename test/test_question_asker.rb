482abdce7faeededddbce556fb51d80357e6cfff
matugm <matugm@gmail.com>
2015-09-23 21:27:46 +0200

    Make test more useful

--- test/test_question_asker.rb
+++ test/test_question_asker.rb
@@ -4,14 +4,19 @@ require_relative "../lib/highline/question_asker"
 
 class TestQuestion < Minitest::Test
   def setup
-    @highline = HighLine.new
+    @input    = StringIO.new
+    @output   = StringIO.new
+    @highline = HighLine.new(@input, @output)
 
     @question = HighLine::Question.new("How are you?", nil)
     @asker    = HighLine::QuestionAsker.new(@question, @highline)
   end
 
   def test_ask_once
-    skip
-    assert_equal "", @asker.ask_once
+    answer = "Very good, thanks for asking!"
+
+    @input.string = answer
+
+    assert_equal answer, @asker.ask_once
   end
 end
