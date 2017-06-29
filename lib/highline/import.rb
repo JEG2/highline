a7213c16dcd2b3c78ab40ea826c1f2571493e56c
Abinoam P. Marques Jr <abinoam@gmail.com>
2017-06-29 13:32:39 -0300

    Reduce global namespace polution

--- lib/highline/import.rb
+++ lib/highline/import.rb
@@ -22,8 +22,7 @@ require "forwardable"
 #
 module Kernel
   extend Forwardable
-  def_delegators :HighLine, :agree, :ask, :choose, :say,
-                 :use_color=, :use_color?, :reset_use_color
+  def_delegators :HighLine, :agree, :ask, :choose, :say
 end
 
 # When requiring 'highline/import' HighLine adds {#or_ask} to Object so
