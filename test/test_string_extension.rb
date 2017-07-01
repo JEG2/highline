fc3ef73b580f3985e8fb14c85fc9b69e3aa1eac0
Abinoam P. Marques Jr <abinoam@gmail.com>
2017-07-01 10:19:28 -0300

    Whitelist HighLine::String on YAML.safe_load

--- test/test_string_extension.rb
+++ test/test_string_extension.rb
@@ -38,7 +38,7 @@ class TestStringExtension < Minitest::Test
     unless Gem::Version.new(YAML::VERSION) < Gem::Version.new("2.0.2")
       highline_string = HighLine::String.new("Yaml didn't messed with HighLine::String")
       yaml_highline_string = highline_string.to_yaml
-      yaml_loaded_string = YAML.safe_load(yaml_highline_string)
+      yaml_loaded_string = YAML.safe_load(yaml_highline_string, [HighLine::String])
 
       assert_equal "Yaml didn't messed with HighLine::String", yaml_loaded_string
       assert_equal highline_string, yaml_loaded_string
